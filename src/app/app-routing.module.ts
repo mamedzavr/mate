54ef1172d0eb60be2b7dac0e324791241bc18ba8
Mamed Mamedov <mamed.mamedov@telekom.com>
2023-11-02 14:26:15 +0200

    new animations and routing strategy
    Signed-off-by: Mamed Mamedov <mamed.mamedov@telekom.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true , scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
