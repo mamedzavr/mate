f0d1e826529f95ba8ec1570bc893c578bee3081c
Mamed Mamedov <mamed.mamedov@telekom.com>
2023-11-06 11:35:20 +0200

    little refactoring
    Signed-off-by: Mamed Mamedov <mamed.mamedov@telekom.com>

--- src/app/components/menu-list/menu-list.component.ts
+++ src/app/components/menu-list/menu-list.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { Dish } from '../../models/menu.model';
-import { BasketService } from 'src/app/services/basket.service';
+import {Component} from '@angular/core';
+import {Dish} from '../../models/menu.model';
+import {BasketService} from 'src/app/services/basket.service';
+import {animate, state, style, transition, trigger,} from '@angular/animations';
 
 @Component({
   selector: 'app-menu-list',
   templateUrl: './menu-list.component.html',
   styleUrls: ['./menu-list.component.css'],
+  animations: [
+    trigger('slideUpDown', [
+      state(
+        'void',
+        style({
+          height: '0',
+          opacity: '0',
+          overflow: 'hidden',
+        }),
+      ),
+      state(
+        '*',
+        style({
+          height: '*',
+          opacity: '1',
+          overflow: 'auto',
+        }),
+      ),
+      transition('void <=> *', animate('600ms ease-in-out')),
+    ]),
+  ],
 })
 export class MenuListComponent {
+  selectedCategory: string | null = '';
+
   constructor(public basketService: BasketService) {}
 
   soupItems: Dish[] = [
